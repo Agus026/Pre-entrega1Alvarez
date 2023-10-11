6a83d4865a8865d010dd4eb44a045bcbac34608d
agusalvarez <agystinalvarez@gmail.com>
2023-10-10 23:26:15 -0300

    añadiendo css

--- js/app.js
+++ js/app.js
@@ -18,7 +18,39 @@ console.log(`El total de gastos compras fueron de ${contador}`);
 // GASTOS DE LA SEMANA //
 
 
-function Gastos(precio, referencia) {
+function gastosDia (precio, referencia) {
     this.precio = precio;
     this.referencia = referencia;
 }
+
+// const lunes = new gastosDia (5500, "Supermercado")
+// const martes = new gastosDia (2200,"Nafta para el auto")
+// const miercoles = new gastosDia (700,"Comida de la mascota")
+// const jueves = new gastosDia (0,"Hoy no gate nada")
+// const viernes = new gastosDia (1500,"Salida con amigos")
+// const sabado = new gastosDia (150,"Futbol")
+// const domingo = new gastosDia (1800,"Asado de los domingos")
+// arrays de objetos 
+
+    
+
+
+// ARRAYS SEMANA 1
+
+
+const gastosSemana = [
+    {dia: "Lunes", gasto: 5500, referencia: "Supermercado"},
+    {dia: "Martes", gasto: 2200, referencia: "Nafta"},
+    {dia: "Miercoles", gasto: 700, referencia: "Comida mascota"},
+    {dia: "Jueves", gasto: 0, referencia: "Sin gastos"},
+    {dia: "Viernes", gasto: 1500, referencia: "Salida con amigos"},
+    {dia: "Sabado", gasto: 150, referencia: "Futbol"},
+    {dia: "Domingo", gasto: 1800, referencia: "Asado familiar"}   
+];
+
+for (let i = 0; i < gastosSemana.length; i++) {
+    console.log (gastosSemana[i])   
+}
+let lista = document.createElement ("ul");
+lista.classList.add("lista")
+conteni

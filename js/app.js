1b128d92c6bad9b5b9b62bb3cf9de24a53c3c078
agusalvarez <agystinalvarez@gmail.com>
2023-10-13 15:19:51 -0300

    agregando F.oredene superior

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// GASTOS DE LA SEMANA //
 let suma = 0;
 let contador = 0;
 
@@ -15,42 +16,35 @@ while (suma < 100) {
 
 console.log(`La suma total es de :${suma}`);
 console.log(`El total de gastos compras fueron de ${contador}`);
-// GASTOS DE LA SEMANA //
 
 
-function gastosDia (precio, referencia) {
-    this.precio = precio;
-    this.referencia = referencia;
-}
-
-// const lunes = new gastosDia (5500, "Supermercado")
-// const martes = new gastosDia (2200,"Nafta para el auto")
-// const miercoles = new gastosDia (700,"Comida de la mascota")
-// const jueves = new gastosDia (0,"Hoy no gate nada")
-// const viernes = new gastosDia (1500,"Salida con amigos")
-// const sabado = new gastosDia (150,"Futbol")
-// const domingo = new gastosDia (1800,"Asado de los domingos")
-// arrays de objetos 
-
-    
 
 
 // ARRAYS SEMANA 1
 
 
-const gastosSemana = [
-    {dia: "Lunes", gasto: 5500, referencia: "Supermercado"},
-    {dia: "Martes", gasto: 2200, referencia: "Nafta"},
-    {dia: "Miercoles", gasto: 700, referencia: "Comida mascota"},
-    {dia: "Jueves", gasto: 0, referencia: "Sin gastos"},
-    {dia: "Viernes", gasto: 1500, referencia: "Salida con amigos"},
-    {dia: "Sabado", gasto: 150, referencia: "Futbol"},
-    {dia: "Domingo", gasto: 1800, referencia: "Asado familiar"}   
+const gastosSemana1 = [
+    { dia: "Lunes", gasto: 5500 },
+    { dia: "Martes", gasto: 2200 },
+    { dia: "Miercoles", gasto: 700 },
+    { dia: "Jueves", gasto: 0 },    
+    { dia: "Viernes", gasto: 1500 },
+    { dia: "Sabado", gasto: 150 },    
 ];
+gastosSemana1.push({ dia: "Domingo", gasto: 1800 });
 
-for (let i = 0; i < gastosSemana.length; i++) {
-    console.log (gastosSemana[i])   
-}
-let lista = document.createElement ("ul");
-lista.classList.add("lista")
-conteni
+ for (let i = 0; i < gastosSemana1.length; i++) {
+     console.log (gastosSemana1[i])   
+ }
+ // Creando lista de gastos de la semana 1
+
+let lista = document.createElement("ul");
+lista.classList.add("lista");
+ for (lugar of gastosSemana1){
+     lista.innerHTML += `<li class= "listaDias"> ${lugar.dia} : $ ${lugar.gasto} </li>`
+ }
+ contenedor.append(lista);
+
+ //RESULTADOS
+const total = gastosSemana1.gastos.reduce((acumulador, elementos)=> acumulador + elementos, 0)
+console.log(total);

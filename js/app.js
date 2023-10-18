515cd9b296d5c5b0718f7a8ddca4a6c7abb706d5
agusalvarez <agystinalvarez@gmail.com>
2023-10-18 20:46:18 -0300

    2PreentregaAlvarez

--- js/app.js
+++ js/app.js
@@ -27,24 +27,32 @@ const gastosSemana1 = [
     { dia: "Lunes", gasto: 5500 },
     { dia: "Martes", gasto: 2200 },
     { dia: "Miercoles", gasto: 700 },
-    { dia: "Jueves", gasto: 0 },    
+    { dia: "Jueves", gasto: 0 },
     { dia: "Viernes", gasto: 1500 },
-    { dia: "Sabado", gasto: 150 },    
+    { dia: "Sabado", gasto: 150 },
 ];
 gastosSemana1.push({ dia: "Domingo", gasto: 1800 });
 
- for (let i = 0; i < gastosSemana1.length; i++) {
-     console.log (gastosSemana1[i])   
- }
- // Creando lista de gastos de la semana 1
+// Creando lista en HTML de gastos de la semana 1
 
 let lista = document.createElement("ul");
 lista.classList.add("lista");
- for (lugar of gastosSemana1){
-     lista.innerHTML += `<li class= "listaDias"> ${lugar.dia} : $ ${lugar.gasto} </li>`
- }
- contenedor.append(lista);
+for (lugar of gastosSemana1) {
+    lista.innerHTML += `<li class= "listaDias"> ${lugar.dia} : $ ${lugar.gasto} </li>`
+}
+contenedor.append(lista);
+// MAP 
+let gastos = gastosSemana1.map ((el)=>el.gasto) 
 
- //RESULTADOS
-const total = gastosSemana1.gastos.reduce((acumulador, elementos)=> acumulador + elementos, 0)
-console.log(total);
+
+//RESULTADOS
+const total = gastos.reduce((acumulador, elementos) => {
+    return acumulador + elementos;
+}, 0)
+
+const contenido = document.querySelector("#contenedor2") 
+let gastosTotales = document.createElement("p");
+contenido.classList.add("texto");
+gastosTotales.innerHTML += ` Los gastos totales de la semana son : ${total} `
+
+contenido.append(gastosTotales);
